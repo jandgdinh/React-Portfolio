a18bc235e0aa5d1457b0235d791d2e20962f1e81
John <jandgdinh@gmail.com>
2024-09-17 18:42:18 -0700

    updated aboutme page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import "../pages/AboutMe.css";
 
 import "../components/Project.css";
 import profilePic from "../assets/profilePic.jpg";
@@ -7,14 +8,14 @@ import profilePic from "../assets/profilePic.jpg";
 export default function AboutMe() {
   return (
     <div className="home">
-      <content>
-        <p>
-          Hi my name is John Dinh and I am a Full Stack Developer from Seattle, WA with a wife and two kids. I have experience with HTML, CSS, JavaScript, Typescript, React, Node.js, Express, PostgreSQL, C, and Python. I am currently looking for a job as a Full Stack Developer or software engineeering intern. 
-        </p>
-        <img src={profilePic} alt="John Dinh" width="500px" />
-        
-        
-      </content>
+      <div className="card">
+        <div className="card-content">
+          <p>
+            Hi my name is John Dinh and I am a Full Stack Developer from Seattle, WA with a wife and two kids. I have experience with HTML, CSS, JavaScript, Typescript, React, Node.js, Express, PostgreSQL, C, and Python. I am currently looking for a job as a Full Stack Developer or software engineering intern.
+          </p>
+          <img src={profilePic} alt="John Dinh" width="500px" />
+        </div>
+      </div>
     </div>
   );
 }
